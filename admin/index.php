a6d19b4b552e47f8ccaef1032eb66141e0b9c811
efobo <anna_ershova.2002@mail.ru>
2022-06-11 13:03:46 +0300

    first commit

---
+++ admin/index.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>Your ♥ Café - Home page</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+    <body>
+        <!-- Menu Section Starts -->
+        <div class="menu">
+            <div class="wrapper">
+                Menu goes here
+            </div>
+           
+        </div>
+        <!-- Menu Section Ends -->
+
+        <!-- Main Content Section Starts -->
+        <div class="main-content">
+            <div class="wrapper">
+            Main Content goes here
+            </div>
+            
+        </div>
+        <!-- Main Content Section Ends -->
+
+        <!-- Footer Section Starts -->
+        <div class="footer">
+            <div class="wrapper">
+                <p>2022 All rights reserved, Your ♥ Café. Developed by <a href="https://github.com/efobo">efobo</a></p>
+            </div>
+            
+        </div>
+        <!-- Footer Section Ends -->
+    </body>
+</html>
