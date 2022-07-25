2393f581437bc9b3be51cf0807f065ccf787a073
efobo <anna_ershova.2002@mail.ru>
2022-07-25 07:21:41 +0300

    Change-Password Add

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -24,6 +24,18 @@
                         echo $_SESSION['update'];
                         unset($_SESSION['update']);
                     }
+
+                    if (isset($_SESSION['user-not-found']))
+                    {
+                        echo $_SESSION['user-not-found'];
+                        unset($_SESSION['user-not-found']);
+                    }
+
+                    if (isset($_SESSION['change-pwd']))
+                    {
+                        echo $_SESSION['change-pwd'];
+                        unset($_SESSION['change-pwd']);
+                    }
                 ?>
                 <br><br><br>
                 <!-- Button to Add Admin -->
@@ -64,6 +76,7 @@
                                         <td><?php echo $full_name; ?></td>
                                         <td><?php echo $username; ?></td>
                                         <td>
+                                            <a href="<?php echo SITEURL; ?>admin/update-password.php?id=<?php echo $id; ?>" class="btn-secondary">Change Password</a>
                                             <a href="<?php echo SITEURL; ?>admin/update-admin.php?id=<?php echo $id; ?>" class="btn-secondary">Update Admin</a>
                                             <a href="<?php echo SITEURL; ?>admin/delete-admin.php?id=<?php echo $id; ?>" class="btn-danger">Delete Admin</a>
                                             
