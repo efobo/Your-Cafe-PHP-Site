fc30fe4ab24aa1261cebe7ff03292d688cc01af2
efobo <anna_ershova.2002@mail.ru>
2022-07-27 13:10:43 +0300

    Display-Categories Added

--- admin/manage-category.php
+++ admin/manage-category.php
@@ -26,41 +26,98 @@
         <table class="tbl-full">
             <tr>
                 <th>S. N.</th>
-                <th>Full Name</th>
-                <th>Username</th>
+                <th>Title</th>
+                <th>Image</th>
+                <th>Fearured</th>
+                <th>Active</th>
                 <th>Actions</th>
             </tr>
 
-            <tr>
+            <?php
+            
+                $sql = "SELECT * FROM tbl_category";
+
+                $res = mysqli_query($conn, $sql);
+
+                $count = mysqli_num_rows($res);
+
+                $sn = 1;
+
+                if ($count > 0)
+                {
+                    while ($row = mysqli_fetch_assoc($res))
+                    {
+                        $id = $row['id'];
+                        $title = $row['title'];
+                        $image_name = $row['image_name'];
+                        $featured = $row['featured'];
+                        $active = $row['active'];
+
+                        ?>
+
+                        <tr>
+                            <td><?php echo $sn++; ?>. </td>
+                            <td><?php echo $title; ?></td>
+
+                            <td>
+                                <?php
+                                
+                                    if ($image_name != "")
+                                    {
+                                        ?>
+
+                                        <img src="<?php echo SITEURL; ?>img/category/<?php echo $image_name; ?>" width="100px">
+
+                                        <?php
+                                    }
+                                    else
+                                    {
+                                        echo "<div class='error'>Image Not Added</div>";
+                                    }
+
+                                ?>
+                            </td>
+
+                            <td><?php echo $featured; ?></td>
+                            <td><?php echo $active; ?></td>
+                            <td>
+                                <a href="#" class="btn-secondary">Update Category</a>
+                                <a href="#" class="btn-danger">Delete Category</a>
+                    
+                            </td>
+                        </tr>
+
+                        <?php
+                    }
+                }
+                else
+                {
+                    ?>
+
+                    <tr>
+                        <td>
+                            <div class="error" colspan="6">No Category Added</div>
+                        </td>
+                    </tr>
+
+                    <?php
+                }
+            
+            ?>
+            <!-- <tr>
                 <td>1.</td>
                 <td>Anna</td>
                 <td>anna</td>
+                <td></td>
+                <td></td>
                 <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                    <a href="#" class="btn-danger">Delete Admin</a>
+                    <a href="#" class="btn-secondary">Update Category</a>
+                    <a href="#" class="btn-danger">Delete Category</a>
                     
                 </td>
-            </tr>
+            </tr> -->
 
-            <tr>
-                <td>2.</td>
-                <td>Anna</td>
-                <td>anna</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                    <a href="#" class="btn-danger">Delete Admin</a>
-                </td>
-            </tr>
-
-            <tr>
-                <td>3.</td>
-                <td>Anna</td>
-                <td>anna</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                    <a href="#" class="btn-danger">Delete Admin</a>
-                </td>
-            </tr>
+            
         </table>
             
     </div>
