539e4919d23385cd0cbf016b059c3a4ad74f69d0
efobo <anna_ershova.2002@mail.ru>
2022-07-28 10:19:15 +0300

    Delete-Category-Button Added

--- admin/manage-category.php
+++ admin/manage-category.php
@@ -13,6 +13,18 @@
                 echo $_SESSION['add'];
                 unset($_SESSION['add']);
             }
+
+            if (isset($_SESSION['remove']))
+            {
+                echo $_SESSION['remove'];
+                unset($_SESSION['remove']);
+            }
+
+            if (isset($_SESSION['delete']))
+            {
+                echo $_SESSION['delete'];
+                unset($_SESSION['delete']);
+            }
         
         ?>
 
@@ -82,7 +94,7 @@
                             <td><?php echo $active; ?></td>
                             <td>
                                 <a href="#" class="btn-secondary">Update Category</a>
-                                <a href="#" class="btn-danger">Delete Category</a>
+                                <a href="<?php echo SITEURL; ?>admin/delete-category.php?id=<?php echo $id; ?>&image_name=<?php echo $image_name; ?>" class="btn-danger">Delete Category</a>
                     
                             </td>
                         </tr>
@@ -104,18 +116,7 @@
                 }
             
             ?>
-            <!-- <tr>
-                <td>1.</td>
-                <td>Anna</td>
-                <td>anna</td>
-                <td></td>
-                <td></td>
-                <td>
-                    <a href="#" class="btn-secondary">Update Category</a>
-                    <a href="#" class="btn-danger">Delete Category</a>
-                    
-                </td>
-            </tr> -->
+            
 
             
         </table>
