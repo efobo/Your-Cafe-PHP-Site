536c122f8315d95f5558d8a13a6a8cd0ce93d243
efobo <anna_ershova.2002@mail.ru>
2022-08-03 06:13:26 +0300

    Admin-Order_table Added

--- admin/manage-order.php
+++ admin/manage-order.php
@@ -10,41 +10,76 @@
         <table class="tbl-full">
             <tr>
                 <th>S. N.</th>
-                <th>Full Name</th>
-                <th>Username</th>
+                <th>Food</th>
+                <th>Price</th>
+                <th>Qty</th>
+                <th>Total</th>
+                <th>Order Date</th>
+                <th>Status</th>
+                <th>Customer Name</th>
+                <th>Contact</th>
+                <th>Email</th>
+                <th>Address</th>
                 <th>Actions</th>
             </tr>
 
-            <tr>
-                <td>1.</td>
-                <td>Anna</td>
-                <td>anna</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                    <a href="#" class="btn-danger">Delete Admin</a>
-                    
-                </td>
-            </tr>
+            <?php 
+            
+                $sql = "SELECT * FROM tbl_order ORDER BY id DESC";
+                $res = mysqli_query($conn, $sql);
+                $count = mysqli_num_rows($res);
 
-            <tr>
-                <td>2.</td>
-                <td>Anna</td>
-                <td>anna</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                    <a href="#" class="btn-danger">Delete Admin</a>
-                </td>
-            </tr>
+                $sn = 1;
 
-            <tr>
-                <td>3.</td>
-                <td>Anna</td>
-                <td>anna</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                    <a href="#" class="btn-danger">Delete Admin</a>
-                </td>
-            </tr>
+                if ($count > 0)
+                {
+                    while ($row = mysqli_fetch_assoc($res))
+                    {
+                        $id = $row['id'];
+                        $food = $row['food'];
+                        $price = $row['price'];
+                        $qty = $row['qty'];
+                        $total = $row['total'];
+                        $order_date = $row['order_date'];
+                        $status = $row['status'];
+                        $customer_name = $row['customer_name'];
+                        $customer_contact = $row['customer_contact'];
+                        $customer_email = $row['customer_email'];
+                        $customer_address = $row['customer_address'];
+                        ?>
+
+                        <tr>
+                            <td><?php echo $sn++;?></td>
+                            <td><?php echo $food; ?></td>
+                            <td><?php echo $price; ?></td>
+                            <td><?php echo $qty; ?></td>
+                            <td><?php echo $total; ?></td>
+                            <td><?php echo $order_date; ?></td>
+                            <td><?php echo $status; ?></td>
+                            <td><?php echo $customer_name; ?></td>
+                            <td><?php echo $customer_contact; ?></td>
+                            <td><?php echo $customer_email; ?></td>
+                            <td><?php echo $customer_address; ?></td>
+                            <td>
+                                <a href="#" class="btn-secondary">Update Order</a>
+                                <a href="#" class="btn-danger">Delete Order</a>
+                                
+                            </td>
+                        </tr>
+
+                        <?php
+                    }
+                }
+                else
+                {
+                    echo "<tr><td colspan='12' class='error'>Orders Not Available</td></tr>";
+                }
+
+            ?>
+
+           
+
+            
         </table>
     </div>
 </div>
